9cf6ea4f19292ced7ba57f327db5c48fb74af328
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-12-14 20:20:54 -0600

    add

--- car.js
+++ car.js
@@ -1,19 +1,29 @@
 class Car{
-    constructor(x,y,width,height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    constructor(x,y,width,height) { //constructor for Car object
+        this.x = x; //x-coordinate
+        this.y = y; //y-coordinate
+        this.width = width; //car width coordinate
+        this.height = height; //car height coordinate
+        this.controls = new Controls(); //car controls
     }
 
-    draw(context) {
-        context.beginPath();
-        context.rect(
+    update() {
+        if(this.controls.forward){
+            this.y -=2;
+        }
+        if (this.controls.reverse){
+            this.y+=2;
+        }
+    }
+
+    draw(context) { //draw function
+        context.beginPath(); //call the begin path function
+        context.rect( //we are going to draw a rectangle
             this.x -this.width/2, //center of the car 
             this.y-this.height/2, //height of the car
-            this.width,
-            this.height
+            this.width, //width 
+            this.height //height
         );
-        context.fill();
+        context.fill(); //draw the car
     }
 }
--- main.js
+++ main.js
@@ -4,4 +4,13 @@ canvas.width = 300;
 
 const context = canvas.getContext('2d');
 const car = new Car(100,100,30,50);
-car.draw(context)
+
+animate();
+
+function animate() {
+    car.update();
+
+    canvas.height = window.innerHeight;
+    car.draw(context);
+    requestAnimationFrame(animate);
+}
